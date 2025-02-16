da42956cb45dde3b350e2f37a7c243505fa6e5f0
Armin Biere <armin.biere@gmail.com>
2025-02-16 15:44:24 +0100

    started more negative tests for actual checking

---
+++ test/check/partial/bad/README.md
@@ -0,0 +1,8 @@
+These are expected invalid files for default partial checking.
+
+If you issue `make here` all the failing tests will be run,
+ignoring the failing exit code.  This allows to step through
+all the errors one-by-one and check that error message line
+and column information is correct.
+
+The actual test is `make test` which runs `run.sh`.
--- test/check/partial/bad/run.sh
+++ test/check/partial/bad/run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-path=test/parse/strict/bad
+path=test/check/partial/bad
 name=$path/run.sh
 die () {
   echo "$name: error: $*" 1>&2
@@ -15,8 +15,7 @@ do
   sol=$path/`basename $cnf .cnf`.sol
   [ -f $sol ] || die "could not find '$sol'"
   args="$cnf $sol -q"
-  $binary $args -s 1>/dev/null 2>/dev/null && \
-    die "'dimocheck $args -s' unexpectedly succeeded in strict mode"
-  $binary $args 1>/dev/null || \
-    die "'dimocheck $args' unexpectedly failed in relaxed mode"
+  $binary $args 1>/dev/null 2>/dev/null && \
+    die "'dimocheck $args' unexpectedly succeeded"
 done
+exit 0
