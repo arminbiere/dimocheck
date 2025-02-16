90ed299663e6ae9afa23d1474ec83bf401411a87
Armin Biere <armin.biere@gmail.com>
2025-02-16 08:53:17 +0100

    checking relaxed mode succeeds

--- test/parse/strict/bad/run.sh
+++ test/parse/strict/bad/run.sh
@@ -14,10 +14,12 @@ for cnf in $path/*.cnf
 do
   sol=$path/`basename $cnf .cnf`.sol
   [ -f $sol ] || die "could not find '$sol'"
-  args="$cnf $sol -s -q"
-  if $binary $args 1>/dev/null 2>/dev/null
+  args="$cnf $sol -q"
+  if $binary $args -s 1>/dev/null 2>/dev/null
   then
-    die "'dimocheck $args' unexpectedly succeeded"
+    die "'dimocheck $args -s' unexpectedly succeeded in strict mode"
     exit 1
   fi
+  $binary $args 1>/dev/null || \
+    die "'dimocheck $args' failed in relaxed mode"
 done
