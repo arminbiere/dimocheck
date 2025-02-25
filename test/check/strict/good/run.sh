076777ae2b85cb07cc904e2de370710ff761b501
Armin Biere <armin.biere@gmail.com>
2025-02-25 19:40:37 +0100

    added regressions

---
+++ test/check/strict/good/run.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+path=test/check/strict/good
+name=$path/run.sh
+die () {
+  echo "$name: error: $*" 1>&2
+  exit 1
+}
+cd `dirname $0` || exit 1
+cd ../../../.. || exit 1
+binary=./dimocheck
+[ -f $binary ] || die "could not find 'dimocheck'"
+echo "[running '$name']"
+for cnf in $path/*.cnf
+do
+  sol=$path/`basename $cnf .cnf`.sol
+  [ -f $sol ] || die "could not find '$sol'"
+  args="$cnf $sol -q -s"
+  $binary $args 1>/dev/null || die "'dimocheck $args' failed"
+done
